1e4a0f5e5a85dcdf7878e6e4405ebcba2ac1bf11
Eriq Mendoza <mendozaeriqjohn@yahoo.com>
2020-10-23 14:24:27 +0800

    fix excel header and format using vue-json-excel

--- resources/js/components/pages/inventory/reportContainer.vue
+++ resources/js/components/pages/inventory/reportContainer.vue
@@ -12,17 +12,15 @@
                     :label="`Filtered by: ${switchLabel}`"
                 >
                 </v-switch>
-                <vue-excel-xlsx
-                    v-if="!toggle.state"
-                    type="button"
+                <app-download-excel
                     class="pink--text text--accent-2 v-btn v-btn--flat v-btn--text theme--light v-size--default"
-                    :data="excelData"
-                    :columns="columns"
-                    :filename="filename"
-                    :sheetname="filename"
+                    :name="header"
+                    :header="header"
+                    :data="data"
+                    :fields="fields"
                 >
                     Download report
-                </vue-excel-xlsx>
+                </app-download-excel>
             </v-col>
         </v-row>
         <app-report-datatable
@@ -33,9 +31,12 @@
     </v-container>
 </template>
 <script>
+import JsonExcel from "vue-json-excel";
+
 export default {
     components: {
-        "app-report-datatable": () => import("./report/datatable.vue")
+        "app-report-datatable": () => import("./report/datatable.vue"),
+        "app-download-excel": JsonExcel
     },
     data() {
         return {
@@ -44,49 +45,35 @@ export default {
                 loading: false,
                 disabled: false
             },
-            filename: "Eshop Beauty N Bottles",
-
-            columns: [
-                {
-                    label: "Code",
-                    field: "code"
-                },
-                {
-                    label: "Raw Item",
-                    field: "name"
-                },
-                {
-                    label: "Purchase",
-                    field: "purchase"
-                },
-                {
-                    label: "RTS",
-                    field: "rts"
-                },
-                {
-                    label: "Sold",
-                    field: "custom_sold"
-                },
-                {
-                    label: "Loss",
-                    field: "custom_loss"
-                },
-                {
-                    label: "Available Units",
-                    field: "units"
-                }
-            ],
+            fields: {
+                Code: "code",
+                "Raw Item": "name",
+                Purchase: "purchase",
+                Rts: "rts",
+                Sold: "custom_sold",
+                Loss: "custom_loss",
+                "Available units": "units"
+            },
             data: [],
-            excelData: []
+            header: null
         };
+    },
+    created() {
+        const options = {
+            year: "numeric",
+            month: "long",
+            day: "numeric"
+        };
+        const today = new Date().toLocaleDateString("en-US", options);
+
+        this.header = `Inventory Report ${today}`;
     },
     methods: {
         toggleChange(value) {
             this.toggle.disabled = this.toggle.loading = value;
         },
         getExcelData(value) {
-            this.data = value;
-            this.excelData = this.data.map(item => {
+            this.data = value.map(item => {
                 item.code = `RI - ${item.id.toString().padStart(4, "0")}`;
                 item.units = `${item.quantity} ${item.base.name}`;
                 item.custom_sold = `(${item.sold})`;
