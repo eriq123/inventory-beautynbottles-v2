b44552f53ccf3a2b16f2a88651acaf37ade0376c
Eriq Mendoza <mendozaeriqjohn@yahoo.com>
2021-01-07 07:28:53 +0800

    added logs to raw add and update for accurate report

--- app/Http/Controllers/Products/RawController.php
+++ app/Http/Controllers/Products/RawController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers\Products;
 
 use App\Convert;
 use App\Http\Controllers\Controller;
+use App\Log;
 use App\Raw;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class RawController extends Controller
 {
+    private $log;
     public function view(Request $request)
     {
         if ($request->id) {
@@ -33,24 +37,51 @@ class RawController extends Controller
 
     public function store(Request $request)
     {
-        $this->validation($request);
-        $this->data['raw'] = new Raw();
-        $this->data['raw']->qr_code = "eshopbnb-RI-";
-        $this->saveRawInformation($request);
+        DB::transaction(function () use ($request) {
+            $this->validation($request);
 
-        $this->data['raw']->update(['qr_code' => 'eshopbnb-RI-' . $this->data['raw']->id]);
+            $this->data['raw'] = new Raw();
+            $this->data['raw']->qr_code = "eshopbnb-RI-";
+            $this->saveRawInformation($request);
 
-        $this->prepareRaw();
+            $this->data['raw']->update(['qr_code' => 'eshopbnb-RI-' . $this->data['raw']->id]);
+
+            $this->prepareRaw();
+            $this->addToLogs($request, 'store');
+        });
         return response()->json($this->data);
+    }
+
+    private function addToLogs($request, $action)
+    {
+        $quantity = $request->quantity - $this->data['raw']->purchase;
+        if ($quantity !== 0) {
+            $this->log = new Log();
+            $this->log->user_id = Auth::id();
+            $this->log->raw_id = $this->data['raw']->id;
+            if ($action == 'store') {
+                $this->log->quantity = $request->quantity;
+                $this->data['raw']->update(['purchase' => $request->quantity]);
+            } else {
+                $this->log->quantity = $quantity;
+                $this->data['raw']->update(['purchase' => $this->data['raw']->purchase + $quantity]);
+            }
+            $this->log->status = 'Purchase';
+            $this->log->save();
+        }
     }
 
     public function update(Request $request)
     {
-        $this->validation($request);
-        $this->data['raw'] = Raw::findorFail($request->id);
-        $this->saveRawInformation($request);
+        DB::transaction(function () use ($request) {
+            $this->validation($request);
 
-        $this->prepareRaw();
+            $this->data['raw'] = Raw::findorFail($request->id);
+            $this->saveRawInformation($request);
+
+            $this->prepareRaw();
+            $this->addToLogs($request, 'update');
+        });
         return response()->json($this->data);
     }
 
--- app/Raw.php
+++ app/Raw.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Raw extends Model
 {
     use SoftDeletes;
-    protected $fillable = ['qr_code'];
+    protected $fillable = ['qr_code', 'purchase'];
 
     public function getNameAttribute($value)
     {
